a705c365384994233653ef00ba8d6ec7e515758c
MinjeMun <mmj010220@gmail.com>
2023-01-25 16:27:11 +0900

    feat: Order List Page
    주문 리스트 페이지 (주문번호, 주문일자 출력)

--- shinhantoy/order/views.py
+++ shinhantoy/order/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from .models import Order
+
+from .serializers import OrderSerializer
+from.paginations import OrderLargePagination
+
 
 # Create your views here.
+
+class OrderListView(
+    mixins.ListModelMixin, 
+    generics.GenericAPIView
+):
+    serializer_class = OrderSerializer
+    pagination_class = OrderLargePagination
+
+    def get_queryset(self):
+        return Order.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        return self.list(self, request, args, kwargs)
