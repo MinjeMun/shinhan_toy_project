9f2b8b1fe58f874bdd49a85dd9f7329a54aa549a
MinjeMun <mmj010220@gmail.com>
2023-01-26 15:00:31 +0900

    feat: order detail page
    주문 상세 내역 페이지 생성함

--- shinhantoy/order/serializers.py
+++ shinhantoy/order/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Order
 
 class OrderSerializer(serializers.ModelSerializer):
-    pass
 
     class Meta:
         model = Order
--- shinhantoy/order/views.py
+++ shinhantoy/order/views.py
@@ -20,3 +20,27 @@ class OrderListView(
 
     def get(self, request, *args, **kwargs):
         return self.list(self, request, args, kwargs)
+
+class OrderDetailView(
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView
+):
+    serializer_class = OrderSerializer
+
+    def get_queryset(self):
+        return Order.objects.all().order_by('id')
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, args, kwargs)
+
+# class CommentCreateView(
+#     mixins.CreateModelMixin,
+#     generics.GenericAPIView
+# ):
+#     serializer_class = CommentSerializer
+
+#     def get_queryset(self):
+#         return Comment.objects.all()
+
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, args, kwargs)
